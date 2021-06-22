ce3e8b5af16ba37aa1fbdae0ec7a66f9383c65f0
Ahtasham <ahtashamfirdausi99@hotmail.com>
2021-06-23 01:39:58 +0530

    code updated

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ provider "aws" {
 data "aws_vpc" "default" {
  default = true   
  }
- data "aws_subnet" "private" {
+ data "aws_subnet" "public" {
  vpc_id = data.aws_vpc.default.id
  filter {
    name = "tag:Name"  
-     values = ["private-eu-west-1a"]    
+     values = ["public-eu-west-1a"]    
    }   
    }
 data "aws_security_groups" "carriyo-sg" {
@@ -44,11 +44,11 @@ data "aws_ami" "latest-amazon-linux-image"{
 resource "aws_instance" "carriyo-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
-    subnet_id = data.aws_subnet.private.id
+    subnet_id = data.aws_subnet.public.id
     vpc_security_group_ids = [data.aws_security_groups.carriyo-sg.ids[0]]
     availability_zone = var.avail_zone
     associate_public_ip_address = true
-//  key_name = aws_key_pair.ssh-key.id
+ // key_name = aws_key_pair.ssh-key.id
     key_name = "carriyo-key"
     user_data = file("entry-script.sh")
     tags ={
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "aws_vpc_default_id"{
   value = data.aws_vpc.default.id
 }
-output "aws_subnet_carriya_private_id"{
-  value = data.aws_subnet.private.id
+output "aws_subnet_carriya_public_id"{
+  value = data.aws_subnet.public.id
 }
 output "carriyo_sg_id"{
     value =data.aws_security_groups.carriyo-sg.ids[0]
