51d6210ca970ea0a7f7adf454a4cd6b678336455
Ahtasham <ahtashamfirdausi99@hotmail.com>
2021-06-22 23:49:43 +0530

    key-pair updated

--- main.tf
+++ main.tf
@@ -1,21 +1,26 @@
 provider "aws" {
-
-    region = "eu-west-1"
-
+ region = "eu-west-1"
 }
 data "aws_vpc" "default" {
  default = true   
  }
- 
  data "aws_subnet" "private" {
  vpc_id = data.aws_vpc.default.id
  filter {
    name = "tag:Name"  
      values = ["private-eu-west-1a"]    
    }   
-   
    }
-
+data "aws_security_groups" "carriyo-sg" {
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
 data "aws_ami" "latest-amazon-linux-image"{
     most_recent = true
     owners = ["amazon"]
@@ -31,37 +36,22 @@ data "aws_ami" "latest-amazon-linux-image"{
     name   = "root-device-type"
     values = ["ebs"]
   }
-    
 }
-
-
-resource "aws_key_pair" "ssh-key" {
+/*resource "aws_key_pair" "ssh-key" {
     key_name = "carriyo-ec2-admin"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAnKf8g4Ab9wxeCJ0/t46qWpdQ83Ws756vWSNG+gE8uVClMq1tfPkKto+vg3e2Fauuw2N3D/5D6o50POJBe+VgomttaIy+iCUQpWSMzL57qIraqJ+0CdhD0ItA4G3skRzSWCEHbBKx8Q7kyjQ9T7oUBYJqeyf0Ivk1rCwsDg7BLcf4yrMlHjO+KNps/dLHBFvKsybETgFeSheLx32Jdj09BcRLDdrNWusTK3udT+3W8aF1gG0oVCrHOW/Du4V4hTBuzz3WffCTCa27cDufJHkc6kj+bXVY2cmod9MJlWZ8fJswbki/pT611HClHU7+iMweon8FC5VP2qNkadNIfqNjjQ== rsa-key-20210622"
-  
-}
-data "aws_security_groups" "carriyo-sg" {
-  
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmYObUZLxh7dlCS/SpZB+e4F6+BqWpyAWjNwucjl1Pm7VN5ehpAjmrbnoXnmWK93B0o2FnuZlFfOADYW032t363xJgTD+QPu25lW6KEHxGr1Pu1QbGz5DzLTx95rByCIEOU3GVIcbCLYoqBJce7CqwgA2PRjBbvVW8u/tlfth7kEg+ZmZ4zQfE6BX5hcPpRGPZxJcFLgeJM5f4rLuxS4zVmNPRFEnn9YQET8vKaKFuWtrV/XxAKggDVEyxfQtS5Tlub7uYpgFtm9pRIZ3HOhd8JjyBhPFH73ROHsp1N9DVKOsqEPhCM3Ml+Djgn4i+EWTgSI/a7PJDbpPAgJPcgWZKQ== rsa-key-20210622"
+}*/
 resource "aws_instance" "carriyo-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
-
     subnet_id = data.aws_subnet.private.id
-    vpc_security_group_ids = [data.aws_security_groups.carriyo-sg.id]
+    vpc_security_group_ids = [data.aws_security_groups.carriyo-sg.ids[0]]
     availability_zone = var.avail_zone
-
     associate_public_ip_address = true
+//  key_name = aws_key_pair.ssh-key.id
     key_name = "carriyo-key"
-
     user_data = file("entry-script.sh")
-
     tags ={
         Name: "${var.env_prefix}-carriyo-server"
     }
-  
 }
--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,9 @@
 output "aws_vpc_default_id"{
   value = data.aws_vpc.default.id
 }
-
 output "aws_subnet_carriya_private_id"{
   value = data.aws_subnet.private.id
 }
 output "carriyo_sg_id"{
-    value =data.aws_security_groups.carriyo-sg.id
+    value =data.aws_security_groups.carriyo-sg.ids[0]
 }
